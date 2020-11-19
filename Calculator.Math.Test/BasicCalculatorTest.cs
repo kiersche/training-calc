3bcc2d583ecbe6c5be9c136da2efe46a0dcaf829
Stefan Kirschner <s-kirschner@web.de>
2020-11-19 17:13:11 +0100

    Trying out teaching method and keeping an eye on the clock

--- Calculator.Math.Test/BasicCalculatorTest.cs
+++ Calculator.Math.Test/BasicCalculatorTest.cs
@@ -6,9 +6,15 @@ namespace Calculator.Math.Test
     public class BasicCalculatorTest
     {
         [Fact]
-        public void Test1()
+        public void CanAdd()
         {
+            const double x = 3.5d;
+            const double y = 2.5d;
 
+            var calculator = new BasicCalculator();
+            double result = calculator.Calculate(x, y, BasicOperation.Add);
+
+            Assert.Equal(6.0d, result);
         }
     }
 }
--- Calculator.Math/BasicCalculator.cs
+++ Calculator.Math/BasicCalculator.cs
@@ -6,6 +6,11 @@ namespace Calculator.Math
     {
         public double Calculate(double x, double y, BasicOperation operation)
         {
+            if (operation == BasicOperation.Add)
+            {
+                return x + y;
+            }
+
             throw new NotImplementedException();
         }
     }
