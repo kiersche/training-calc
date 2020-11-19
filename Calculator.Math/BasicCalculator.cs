92d3f364c69cc40b490e098089074f154d88a408
Stefan Kirschner <s-kirschner@web.de>
2020-11-19 17:04:08 +0100

    Added Calculate method that accepts a BasicOperation value to determine what should be done with x and y

--- Calculator.Math/BasicCalculator.cs
+++ Calculator.Math/BasicCalculator.cs
@@ -4,5 +4,9 @@ namespace Calculator.Math
 {
     public class BasicCalculator
     {
+        public double Calculate(double x, double y, BasicOperation operation)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Calculator.Math/BasicOperation.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Math
+{
+    public enum BasicOperation { Add, Subtract, Multiply, Divide }
+}
