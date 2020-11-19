3658ed693bad1db35b500492c7b2abaed858156f
Stefan Kirschner <s-kirschner@web.de>
2020-11-19 16:57:52 +0100

    Added basic stubs

---
+++ Calculator.Math.Test/BasicCalculatorTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Calculator.Math.Test
+{
+    public class BasicCalculatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
